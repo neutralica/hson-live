a87817d20d84e382c2b0a0c44df0182c062fe783
Phillip Hanson <hansonpw@gmail.com>
2026-04-05 18:32:07 -0500

    the most fun! with indices; creating _test exports

---
+++ src/_tests/test-exports.ts
@@ -0,0 +1,5 @@
+
+export { pseudo_to_suffix } from "../api/livetree/managers/css-manager.js";
+export  { normalize_css_value } from "../utils/attrs-utils/normalize-css.js";
+export { normalize_css_key } from "../utils/attrs-utils/normalize-css.js";
+export { canon_to_css_prop } from "../utils/attrs-utils/normalize-css.js";
--- src/diagnostics/index.ts
+++ src/diagnostics/index.ts
@@ -1,5 +1,6 @@
 // /diagnostics/index.ts
 
+
 export { assert_invariants as _assert_invariants } from "./assert-invariants.test.js";
 export { compare_nodes as _compare_nodes } from "./compare-nodes.test.js";
 export { is_Node as _is_Node } from "../utils/node-utils/node-guards.js"
